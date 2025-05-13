2ec2a9e4f97fdaccdfc77ec89bdd13fee58fabd4
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-13 15:52:46 +1000

    Update pending_app.py

--- pending_app.py
+++ pending_app.py
@@ -1,13 +1,16 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
 st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write("Orders that need to be filled.")
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
 if my_dataframe:
